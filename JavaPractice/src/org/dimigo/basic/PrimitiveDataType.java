bbf3e34021ac5c2dc542908005bf2e01a0df894d
dwkim <dwkim@DESKTOP-KSGMHVS>
2017-03-09 11:48:46 +0900

    과제 수정

--- JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
+++ JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
@@ -17,6 +17,7 @@ package org.dimigo.basic;
  */
 public class PrimitiveDataType {
 	public static void main(String[] args) {
+		
 		String name="아이유";
 		boolean isMale=false;
 		int age=25;
@@ -24,18 +25,14 @@ public class PrimitiveDataType {
 		float weight= 44.3f;
 		char bloodType='A';
 		
-		System.out.printf("이름 : %s\n",name);
+		System.out.printf("<<아이유 프로필>>\n이름 : %s\n",name);
 		
 		if(isMale)
 			System.out.printf("성별 : 남자\n");
 		else System.out.printf("성별 : 여자\n");
 		
 		System.out.printf("나이 : %d\n키 : %fcm\n몸무게 : %fkg\n혈액형 : %c형\n\n",age,height,weight,bloodType);
-		System.out.println("성별 : 여자");
-		System.out.println("나이 : " + age);
-		System.out.println("키 : " + height + "cm");
-		System.out.println("몸무게 : " + weight + "kg");
-		System.out.println("혈액형 : " + bloodType + "형");
+
 	}
 
 }
