8d9a805a371ba87cb50fb0b5c53caba7e3245a7b
dwkim <dwkim@DESKTOP-KSGMHVS>
2017-04-26 12:51:41 +0900

    실습과제 9 제출

---
+++ JavaPractice/src/org/dimigo/inheritance/Chinese.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *    |- Korean
+ *
+ * 1. 개요 :
+ * 2. 작성일: Apr 26, 2017
+ * </pre>
+ *
+ * @author dwkim
+ * @version : 1.0
+ */
+public class Chinese extends Person{
+	
+	public Chinese(String name){
+		super(name);
+	}
+	public void sayHello(){
+	System.out.println("니하오");	
+	}
+	public void sayBye(){
+		System.out.println("짜이찌엔");
+	}
+	public String toString(){
+		return "저는 중국사람 " + getName() + "입니다.";
+	}
+}
